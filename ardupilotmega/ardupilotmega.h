6f4ebdb83dd9dcd9b8263f52b7d8c0bd93df3f6b
ThePeach-kr <peach@thepeach.kr>
2022-09-27 05:54:33 +0000

    autogenerated headers for rev https://github.com/the-peach-drone/mavlink/tree/2cdc6254ebaf45ddbe2e7d290f9acf42bd908276

--- ardupilotmega/ardupilotmega.h
+++ ardupilotmega/ardupilotmega.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ARDUPILOTMEGA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ARDUPILOTMEGA_XML_HASH 7513536935726088505
+#define MAVLINK_ARDUPILOTMEGA_XML_HASH 6160912972435780534
 
 #ifdef __cplusplus
 extern "C" {
