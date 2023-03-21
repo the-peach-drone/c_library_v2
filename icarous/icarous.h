69f80bf2ca1398f02f02e4de7c577f22d0605dca
ThePeach-kr <peach@thepeach.kr>
2023-03-21 02:51:35 +0000

    autogenerated headers for rev https://github.com/the-peach-drone/mavlink/tree/e77b0a3df5a0c95f3944b9432b17ea5f2bc784de

--- icarous/icarous.h
+++ icarous/icarous.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ICAROUS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ICAROUS_XML_HASH 5917617156528006531
+#define MAVLINK_ICAROUS_XML_HASH -5757942343648989843
 
 #ifdef __cplusplus
 extern "C" {
--- test/test.h
+++ test/test.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_TEST.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_TEST_XML_HASH 2687595664061676961
+#define MAVLINK_TEST_XML_HASH 8751272041129561466
 
 #ifdef __cplusplus
 extern "C" {
